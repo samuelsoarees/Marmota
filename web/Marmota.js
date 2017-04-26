5391d7cc5366050eb51656c0e671df43617e664b
andersonf007 <andersonfseabra3@outlook.com>
2017-04-26 19:52:18 -0300

    adicionado metodo de criacao randomica

---
+++ web/Marmota.js
@@ -0,0 +1,28 @@
+
+function createCookieRandon(){
+    var alturaJanela = window.innerHeight;
+    var larguraJanela = window.innerWidth;
+    var topCookie = 0;
+    var leftCookie = 0;
+    topCookie = getRandomInt(50, alturaJanela - 100);
+    leftCookie = getRandomInt(50, larguraJanela - 150);
+    
+    rndID = getRandomInt(50, 100);
+    
+    var cookie = document.createElement("div");
+    var cookieId = document.createAttribute("id");
+    var cookieClass = document.createAttribute('class');
+    
+    
+    cookieId.value = rndID;
+    cookieClass.value = 'cookie';
+    
+    cookie.setAttributeNode(cookieId);
+    cookie.setAttributeNode(cookieClass);
+    
+    cookie.setAttribute("style","width:50px;height:50px;background:green;top:"+topCookie+"px;left:"+leftCookie+"px;position:absolute");
+    
+    
+    document.body.appendChild(cookie);
+    
+}
